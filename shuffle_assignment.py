ddc1cfba5344cdebed5007480930bcad1c9fb936
Shumo Chu <shumo.chu@gmail.com>
2014-05-02 02:03:50 -0700

    add output to WCNF.

---
+++ shuffle_assignment.py
@@ -0,0 +1,90 @@
+from operator import mul
+import itertools
+
+
+class ShuffleAssignment(object):
+
+    def __init__(self,
+                 num_servers,
+                 child_sizes,
+                 hc_dim_sizes,
+                 join_field_map):
+        # validate
+        if len(hc_dim_sizes) != len(join_field_map):
+            raise Exception("hc_dim_sizes must match the \
+                             length of join_field_map")
+        # init variables
+        self.num_servers = num_servers
+        self.child_sizes = child_sizes
+        self.hc_dim_sizes = hc_dim_sizes
+        self.join_field_map = join_field_map
+        self.num_vs = reduce(mul, self.hc_dim_sizes, 1)
+        # r_index[i]: indexes of joined fields that is related to relation i
+        self.r_index = [[] for i in range(0, len(child_sizes))]
+        # compute voxel numbers
+        self.voxel_numbers = []
+        for i, jf_list in enumerate(join_field_map):
+            for jf in jf_list:
+                self.r_index[jf[0]].append(i)
+        for i, size in enumerate(child_sizes):
+            scale = 1
+            for index in self.r_index[i]:
+                scale = scale*hc_dim_sizes[index]
+            self.voxel_numbers.append(scale)
+        # compute voxel sizes
+        self.normalized_voxel_sizes = []
+        for i, num in enumerate(self.voxel_numbers):
+            self.normalized_voxel_sizes.append(child_sizes[i]*self.num_vs/num)
+
+    def variable(self, vs, rs):
+        """ Return a variable indicating virtual server vs is assigned to
+            real server rs.
+        """
+        return self.num_vs*(rs-1) + vs
+
+    def coordinate_to_vs(self, coordinate):
+        """ Return the virtual server id of the coordinate
+        """
+        if len(coordinate) != len(self.hc_dim_sizes):
+            raise Exception(
+                "coordinate has {} dims yet hc has {}".format(
+                    len(coordinate), len(self.hc_dim_sizes)))
+        vs = 0
+        for i, s in enumerate(self.hc_dim_sizes):
+            vs = vs * s + coordinate[i]
+        return vs+1
+
+    def voxel_to_vs(self, vox):
+        """ Return an array of virtual servers which are mapped to a vox.
+        """
+        # helper function: combine two part of coordinates
+        def full_coordinate(joined, unjoined):
+            coordinates = sorted(joined+unjoined, key=lambda co: co[0])
+            return tuple(map(lambda x: x[1], coordinates))
+
+        # 1. coordinates of joined part, specified by the input
+        joined = zip(self.r_index[vox["rel"]], vox["dims"])
+        # 2. get the ranges for iterations of unjoined part
+        unjoined_dims = [dim for dim in range(0, len(self.hc_dim_sizes))
+                         if dim not in self.r_index[vox["rel"]]]
+        unjoined_ranges = [range(0, self.hc_dim_sizes[dim]) for dim
+                           in unjoined_dims]
+        # 3. get the coordinates
+        coordinates = [full_coordinate(joined, zip(unjoined_dims, subcube_co))
+                       for subcube_co in itertools.product(*unjoined_ranges)]
+        return [self.coordinate_to_vs(co) for co in coordinates]
+
+    def get_wcnf(self):
+        """ print problem model in WCNF (Weighted Conjunctive Normal Format)
+        """
+        max_size = reduce(mul, self.child_sizes, 1)
+        wcnf = []
+        # hard constraints: each virtual server must be assigned
+        for vs in range(1, self.num_vs+1):
+            l = [max_size]
+            l.extend([self.variable(vs, rs)
+                      for rs in range(1, self.num_servers+1)])
+            l.append(0)
+            wcnf.append(l)
+        # soft constraints: penalty on work load on each server
+        return wcnf
--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 import optimizer
 import unittest
 import numpy as np
+from shuffle_assignment import ShuffleAssignment
 
 ''' unit test of optimizer
 '''
@@ -9,47 +10,68 @@ import numpy as np
 
 class TestOpimizerFunctions(unittest.TestCase):
 
-    def testWorkLoad1(self):
-        joinFieldMap = [[[0, 1], [1, 0]], [[1, 1], [2, 0]], [[0, 0], [2, 1]]]
-        dimSizes = [3, 3, 3]
-        childSizes = [27, 9, 9]
+    def test_workload_1(self):
+        join_field_map = [[[0, 1], [1, 0]], [[1, 1], [2, 0]], [[0, 0], [2, 1]]]
+        dim_sizes = [3, 3, 3]
+        child_sizes = [27, 9, 9]
         self.assertEqual(
-            optimizer.workLoad(dimSizes, childSizes, joinFieldMap), 5)
+            optimizer.workload(dim_sizes, child_sizes, join_field_map), 5)
 
-    def testWorkLoad2(self):
-        joinFieldMap = [[[0, 1], [1, 0]], [[1, 1], [2, 0]]]
+    def test_workload_2(self):
+        join_field_map = [[[0, 1], [1, 0]], [[1, 1], [2, 0]]]
         dimSizes = [4, 6]
-        childSizes = [24, 36, 48]
+        child_sizes = [24, 36, 48]
         self.assertEqual(
-            optimizer.workLoad(dimSizes, childSizes, joinFieldMap),
+            optimizer.workload(dimSizes, child_sizes, join_field_map),
             24.0 / 4.0 + 36.0 / 24.0 + 48.0 / 6.0)
 
-    def testOpimizer(self):
+    def test_optimizer(self):
         ''' Result(x,y,z,u,v) :- R(x,y), S(y,z), T(z,u), P(u,v)
         '''
-        joinFieldMap = [[[0, 1], [1, 0]], [[1, 1], [2, 0]],
-                        [[2, 1], [3, 0]]]
-        childSizes = [36, 72, 64, 36]
+        join_field_map = [[[0, 1], [1, 0]],
+                          [[1, 1], [2, 0]],
+                          [[2, 1], [3, 0]]]
+        child_sizes = [36, 72, 64, 36]
         self.assertEqual(
-            optimizer.getDimSizesBFS(64, childSizes, joinFieldMap),
-            optimizer.getDimSizesDFS(64, childSizes, joinFieldMap)
+            optimizer.get_dim_sizes_bfs(64, child_sizes, join_field_map),
+            optimizer.get_dim_size_dfs(64, child_sizes, join_field_map)
         )
 
-    '''
-    def testOptFracDimSize(self):
-        joinFieldMap = [[[0, 1], [1, 0]], [[1, 1], [2, 0]],
-                        [[2, 1], [3, 0]]]
-        childSizes = [36, 72, 64, 36]
-        optimizer.optDimFracSize(64, childSizes, joinFieldMap)
-    '''
+    def test_frac_dim_size(self):
+        join_field_map = [[[0, 1], [1, 0]], [[1, 1], [2, 0]],
+                          [[2, 1], [3, 0]]]
+        child_sizes = [36, 72, 64, 36]
+        optimizer.frac_dim_sizes(64, child_sizes, join_field_map)
 
-    def testOptFracDimSize2(self):
-        joinFieldMap = [[[0, 1], [1, 0]], [[1, 1], [2, 0]], [[0, 0], [2, 1]]]
-        childSizes = [500, 500, 500]
-        (obj, shares) = optimizer.optDimFracSize(64, childSizes, joinFieldMap)
+    def test_frac_dim_size_2(self):
+        join_field_map = [[[0, 1], [1, 0]], [[1, 1], [2, 0]], [[0, 0], [2, 1]]]
+        child_sizes = [500, 500, 500]
+        (obj, shares) = optimizer.frac_dim_sizes(64, child_sizes,
+                                                 join_field_map)
         np.testing.assert_almost_equal(obj, 0.82763333)
         np.testing.assert_almost_equal(
             shares, [0.33333333, 0.33333333, 0.33333333])
+
+    def test_shuffle_assignment(self):
+        def assert_list_of_set(a, b):
+            self.assertEqual(map(set, a), map(set, b))
+        join_field_map = [[[0, 1], [1, 0]],
+                          [[2, 0], [1, 1]],
+                          [[0, 0], [2, 1]]]
+        child_sizes = [500, 500, 500]
+        model = ShuffleAssignment(64, child_sizes,
+                                  [10, 20, 10], join_field_map)
+        # test r_index
+        assert_list_of_set(model.r_index, [[2, 0], [0, 1], [1, 2]])
+        self.assertEqual(model.voxel_numbers, [100, 200, 200])
+        self.assertEqual(model.coordinate_to_vs((9, 19, 9)), 2000)
+        self.assertEqual(model.normalized_voxel_sizes, [10000, 5000, 5000])
+        print len(model.get_wcnf())
+        vox = {
+            "rel": 0,
+            "dims": [0, 0]
+        }
+        print model.voxel_to_vs(vox)
 
 
 if __name__ == '__main__':
